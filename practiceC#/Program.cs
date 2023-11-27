dea4e8aaae3ddc9ad685f86f0c7fdaad20b50bbd
CordJG <wjrmffldglem@gmail.com>
2023-11-27 14:50:52 +0900

    test commit

---
+++ practiceC#/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace HelloWrold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("뭐라는겨");
+        }
+    }
+    
+
+}
+
